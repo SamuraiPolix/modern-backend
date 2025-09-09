2b67a958fe0668c92ac9a974884d2bc8155fe3ce
Samuel Lazareanu <sam.lazareanu@gmail.com>
2025-09-09 15:52:42 +0300

    init

---
+++ src/main/java/com/samlazfig/modern_backend_meetup/ModernBackendMeetupNivApplication.java
@@ -0,0 +1,13 @@
+package com.samlazfig.modern_backend_meetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernBackendMeetupNivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernBackendMeetupNivApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samlazfig/modern_backend_meetup/ModernBackendMeetupNivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samlazfig.modern_backend_meetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernBackendMeetupNivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
